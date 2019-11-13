a7045fe07ebf6df76cbc6853dd628a9068790fa2
Budi Kusuma Utama <budi@Budis-MacBook-Pro.local>
2019-11-13 20:12:12 +0700

    update

--- modules/index.js
+++ modules/index.js
@@ -8,8 +8,8 @@ const config = require('../config/kafka');
     console.log("consumer running")
     console.log(config)
     try {
-        const Consumer = kafka.Consumer;
-        const client = new kafka.KafkaClient(config.kafka_server);
+        const Consumer = kafka.HighLevelConsumer;
+        const client = new kafka.Client(config.kafka_server);
         let consumer = new Consumer(
           client,
           [{ topic: config.kafka_topic, partition: 0, offset: -1}],
@@ -28,29 +28,29 @@ const config = require('../config/kafka');
             'kafka : ',
             message.value
           );
-          
-          let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-              user: config.sender_email,
-              pass: config.sender_password
-            }
-          });
-          let mailOptions = {
-            from: config.sender_name +'<'+config.sender_email+'>',
-            to: config.destination_email,
-            subject: `Message from ` + config.kafka_topic,
-            text: message.value
-          };
-          transporter.sendMail(mailOptions, function(error, info) {
+          ///Please use Valid SMTP
+          // let transporter = nodemailer.createTransport({
+          //   service: 'gmail',
+          //   auth: {
+          //     user: config.sender_email,
+          //     pass: config.sender_password
+          //   }
+          // });
+          // let mailOptions = {
+          //   from: config.sender_name +'<'+config.sender_email+'>',
+          //   to: config.destination_email,
+          //   subject: `Message from ` + config.kafka_topic,
+          //   text: message.value
+          // };
+          // transporter.sendMail(mailOptions, function(error, info) {
       
-            if (error) {
-              throw error;
-            } else {
-              console.log('Email successfully sent!');
+          //   if (error) {
+          //     throw error;
+          //   } else {
+          //     console.log('Email successfully sent!');
               
-            }
-          });
+          //   }
+          // });
         })
         consumer.on('error', function(err) {
           console.log('error', err);
