3d14ff824d00a2e9fb593d3f988964680dbbb731
Budi Kusuma Utama <budi@Budis-MacBook-Pro.local>
2019-11-13 20:24:21 +0700

    update

--- modules/index.js
+++ modules/index.js
@@ -8,8 +8,8 @@ const config = require('../config/kafka');
     console.log("consumer running")
     console.log(config)
     try {
-      const Consumer = kafka.HighLevelConsumer;
-      const client = new kafka.Client(config.kafka_server);
+      const Consumer = kafka.Consumer;
+      const client = new kafka.KafkaClient(config.kafka_server);
       let consumer = new Consumer(
         client,
         [{ topic: config.kafka_topic, partition: 0 }],
