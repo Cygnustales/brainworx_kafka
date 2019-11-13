6b2bda31ae9a293c7125bc41b01cbe9a036c88a4
Budi Kusuma Utama <budi@Budis-MacBook-Pro.local>
2019-11-13 15:55:22 +0700

    update

--- config/kafka.js
+++ config/kafka.js
@@ -1,7 +1,6 @@
 
 module.exports = {
-    // kafka_topic: "simptest.unwrap2.dbo.Transaction",
-    kafka_topic: "TXN_W",
+    kafka_topic: "simptest.unwrap2.dbo.Transaction",
     kafka_server: "zb-vm1:2181",
     sender_email: "emailkirim360@gmail.com",
     sender_password: "formytest",
